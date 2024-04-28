43ac4bf6b9df6117304083038bfe80edb5dc4eeb
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-28 19:54:11 +0530

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -4,37 +4,32 @@ import asyncio
 from PIL import Image
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
+from remname import remove_text_from_filename  # Import the necessary function from remname.py
 
 async def fix_thumb(thumb):
     width = 0
     height = 0
     try:
-        if thumb != None:
+        if thumb is not None:
             parser = createParser(thumb)
             metadata = extractMetadata(parser)
             if metadata.has("width"):
                 width = metadata.get("width")
             if metadata.has("height"):
                 height = metadata.get("height")
-                
-            # Open the image file
+
             with Image.open(thumb) as img:
-                # Convert the image to RGB format and save it back to the same file
                 img.convert("RGB").save(thumb)
-            
-                # Resize the image
                 resized_img = img.resize((width, height))
-                
-                # Save the resized image in JPEG format
                 resized_img.save(thumb, "JPEG")
             parser.close()
     except Exception as e:
         print(e)
-        thumb = None 
-       
+        thumb = None
+
     return width, height, thumb
-    
-async def take_screen_shot(video_file, output_directory, ttl):
+
+async def take_screen_shot(video_file, output_directory, ttl, remname_text):
     out_put_file_name = f"{output_directory}/{time.time()}.jpg"
     file_genertor_command = [
         "ffmpeg",
@@ -55,5 +50,7 @@ async def take_screen_shot(video_file, output_directory, ttl):
     e_response = stderr.decode().strip()
     t_response = stdout.decode().strip()
     if os.path.lexists(out_put_file_name):
+        # Remove specified text from the output file name using remname.py function
+        out_put_file_name = remove_text_from_filename(out_put_file_name, remname_text)
         return out_put_file_name
     return None
