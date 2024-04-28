6bed98a985b4215d815cb5e930cfbfaf11360c7a
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-28 14:51:50 +0530

    Update remname.py

--- plugins/remname.py
+++ plugins/remname.py
@@ -1,11 +1,11 @@
-from pyrogram import Client
-from database import jishubotz
+from pyrogram import Client, filters
+from helper.database import db
 
 # Create a Pyrogram Client
 app = Client("my_session")
 
 # REMNAME
-@app.on_message(app.filters.private & app.filters.command('set_remname'))
+@app.on_message(filters.private & filters.command('set_remname'))
 async def add_remname(client, message):
     if len(message.command) == 1:
         return await message.reply_text("**__Give The Remname Text__\n\nExample:- `/set_remname text_pattern1, text_pattern2, ...`**")
@@ -16,20 +16,20 @@ async def add_remname(client, message):
     
     SnowDev = await message.reply_text("Please Wait ...")
     for pattern in remname_patterns:
-        await jishubotz.add_remname(message.from_user.id, pattern)
+        await db.add_remname(message.from_user.id, pattern)
     
     await SnowDev.edit("**Remname Text Saved Successfully ✅**")
 
-@app.on_message(app.filters.private & app.filters.command('del_remname'))
+@app.on_message(filters.private & filters.command('del_remname'))
 async def delete_remname(client, message):
     SnowDev = await message.reply_text("Please Wait ...")
-    await jishubotz.delete_remname(message.from_user.id)
+    await db.delete_remname(message.from_user.id)
     await SnowDev.edit("**Remname Text Deleted Successfully 🗑️**")
 
-@app.on_message(app.filters.private & app.filters.command('see_remname'))
+@app.on_message(filters.private & filters.command('see_remname'))
 async def see_remname(client, message):
     SnowDev = await message.reply_text("Please Wait ...")
-    remname_text = await jishubotz.get_remname(message.from_user.id)
+    remname_text = await db.get_remname(message.from_user.id)
     if remname_text:
         await SnowDev.edit(f"**Your Remname Texts:-**\n\n{remname_text}")
     else:
