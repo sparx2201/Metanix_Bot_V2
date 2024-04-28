9b4beb5223fa596385173eb45a3d1f5d0ee99a9c
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-28 14:22:32 +0530

    Update prefix_&_suffix.py

--- plugins/prefix_&_suffix.py
+++ plugins/prefix_&_suffix.py
@@ -67,3 +67,35 @@ async def see_csuffix(client, message):
         await SnowDev.edit(f"**ʏᴏᴜʀ ꜱᴜꜰꜰɪx:-**\n\n`{suffix}`")
     else:
         await SnowDev.edit("__** ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ꜱᴜꜰꜰɪx**__")
+
+
+@Client.on_message(app.filters.private & app.filters.command('set_remname'))
+async def add_remname(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Remname Text__\n\nExample:- `/set_remname text_pattern1, text_pattern2, ...`**")
+    
+    # Get all the text patterns provided in the command
+    remname_text = message.text.split(' ', 1)[1]
+    remname_patterns = [pattern.strip() for pattern in remname_text.split(',')]
+    
+    SnowDev = await message.reply_text("Please Wait ...")
+    for pattern in remname_patterns:
+        await jishubotz.add_remname(message.from_user.id, pattern)
+    
+    await SnowDev.edit("**Remname Text Saved Successfully ✅**")
+
+@Client.on_message(app.filters.private & app.filters.command('del_remname'))
+async def delete_remname(client, message):
+    SnowDev = await message.reply_text("Please Wait ...")
+    await jishubotz.delete_remname(message.from_user.id)
+    await SnowDev.edit("**Remname Text Deleted Successfully 🗑️**")
+
+@Client.on_message(app.filters.private & app.filters.command('see_remname'))
+async def see_remname(client, message):
+    SnowDev = await message.reply_text("Please Wait ...")
+    remname_text = await jishubotz.get_remname(message.from_user.id)
+    if remname_text:
+        await SnowDev.edit(f"**Your Remname Texts:-**\n\n{remname_text}")
+    else:
+        await SnowDev.edit("**You Don't Have Any Remname Texts ❌**")
+
