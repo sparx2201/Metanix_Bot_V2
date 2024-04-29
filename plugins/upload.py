c06dbb893b7cd1cbea519499221c90446e5ca9fc
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-29 22:54:10 +0530

    Update upload.py

--- plugins/upload.py
+++ plugins/upload.py
@@ -2,13 +2,14 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from helper.database import db
 
-ON = [[InlineKeyboardButton('Upload as Document', callback_data='upload_document_on')]]
-OFF = [[InlineKeyboardButton('Upload as Video', callback_data='upload_video_on')]]
+ON = [[InlineKeyboardButton('Upload as Document', callback_data='upload_video_on')]]
+OFF = [[InlineKeyboardButton('Upload as Video', callback_data='upload_document_on')]]
 
+app = Client("upload_example")
 
 @Client.on_message(filters.private & filters.command('upload'))
 async def handle_upload_settings(bot: Client, message: Message):
-    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.message_id)
     upload_type = await db.get_upload_type(message.from_user.id)
     await ms.delete()
     if upload_type == "document":
@@ -18,8 +19,7 @@ async def handle_upload_settings(bot: Client, message: Message):
     else:
         await message.reply_text("Please select the upload format:", reply_markup=InlineKeyboardMarkup(ON))
 
-
-@Client.on_callback_query(filters.regex('(upload_document_on|upload_video_on)'))
+@Client.on_callback_query(filters.regex('(upload_document_on|upload_video_on|upload_document_off|upload_video_off)'))
 async def set_upload_format(bot: Client, query: CallbackQuery):
     data = query.data
     user_id = query.from_user.id
@@ -30,4 +30,5 @@ async def set_upload_format(bot: Client, query: CallbackQuery):
     elif data == 'upload_video_on':
         await db.set_upload_type(user_id, "video")
         await query.message.edit("Upload format set to **Video**.", reply_markup=InlineKeyboardMarkup(OFF))
-   
+
+
