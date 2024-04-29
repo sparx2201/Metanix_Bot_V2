a6cff0e27ca4ee5dee7d8ded5a2b1b1ce312680f
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-29 23:14:15 +0530

    Update upload.py

--- plugins/upload.py
+++ plugins/upload.py
@@ -5,6 +5,8 @@ from helper.database import db
 ON = [[InlineKeyboardButton('Upload as Document', callback_data='upload_document_on')]]
 OFF = [[InlineKeyboardButton('Upload as Video', callback_data='upload_video_on')]]
 
+
+
 @Client.on_message(filters.private & filters.command('upload'))
 async def handle_upload_settings(bot: Client, message: Message):
     ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.message_id)
@@ -28,5 +30,18 @@ async def set_upload_format(bot: Client, query: CallbackQuery):
     elif data == 'upload_video_on':
         await db.set_upload_type(user_id, "video")
         await query.message.edit("Upload format set to **Video**.", reply_markup=InlineKeyboardMarkup(OFF))
+
+@app.on_message(filters.private & (filters.document | filters.video))
+async def handle_media(bot: Client, message: Message):
+    upload_type = await db.get_upload_type(message.from_user.id)
+    ms = await message.reply_text(f"Trying to upload as {upload_type}...")
+    if upload_type == "document" and message.document:
+        await ms.edit(f"Uploading document: {message.document.file_name}")
+        # Implement your document upload logic here
+    elif upload_type == "video" and message.video:
+        await ms.edit(f"Uploading video: {message.video.file_name}")
+        # Implement your video upload logic here
+    else:
+        await ms.edit("Invalid upload format or media type.")
 
 
