e789c530663cd50a59960ca4d89976cd64bc6e1f
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-29 22:34:47 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -20,29 +20,29 @@ ON = [[InlineKeyboardButton('Upload as Document', callback_data='upload_document
 OFF = [[InlineKeyboardButton('Upload as Video', callback_data='upload_video_on')]]
 
 @Client.on_message(filters.private & filters.command('upload'))
-async def handle_upload_settings(bot, message):
-    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+async def handle_upload_settings(bot: Client, message: Message):
+    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.message_id)
     upload_type = await db.get_upload_type(message.from_user.id)
     await ms.delete()
     if upload_type == "document":
-        await message.reply_text(f"Your current upload format is set to **Document**.", reply_markup=InlineKeyboardMarkup(ON))
+        await message.reply_text("Your current upload format is set to **Document**.", reply_markup=InlineKeyboardMarkup(OFF))
     elif upload_type == "video":
-        await message.reply_text(f"Your current upload format is set to **Video**.", reply_markup=InlineKeyboardMarkup(OFF))
+        await message.reply_text("Your current upload format is set to **Video**.", reply_markup=InlineKeyboardMarkup(ON))
     else:
         await message.reply_text("Please select the upload format:", reply_markup=InlineKeyboardMarkup(ON))
 
-@Client.on_callback_query(filters.regex('(upload_document_on|upload_video_on|upload_document_off|upload_video_off)'))
-async def set_upload_format(bot, query):
+@Client.on_callback_query(filters.regex('(upload_document_on|upload_video_on)'))
+async def set_upload_format(bot: Client, query: CallbackQuery):
     data = query.data
     user_id = query.from_user.id
 
     if data == 'upload_document_on':
         await db.set_upload_type(user_id, "document")
-        await query.message.edit("Upload format set to **Document**.", reply_markup=InlineKeyboardMarkup(ON))
+        await query.message.edit("Upload format set to **Document**.", reply_markup=InlineKeyboardMarkup(OFF))
     elif data == 'upload_video_on':
         await db.set_upload_type(user_id, "video")
-        await query.message.edit("Upload format set to **Video**.", reply_markup=InlineKeyboardMarkup(OFF))
-
+        await query.message.edit("Upload format set to **Video**.", reply_markup=InlineKeyboardMarkup(ON))
+        
 @Client.on_message(filters.private & filters.reply)
 async def refunc(client, message):
     reply_message = message.reply_to_message
