f3d4fa5ba33a59f3ae64fa5ae6f0d5341134ff9f
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-30 19:08:08 +0530

    Update upload.py

--- plugins/upload.py
+++ plugins/upload.py
@@ -8,7 +8,7 @@ ON = [[InlineKeyboardButton('Upload as Document', callback_data='upload_document
       [InlineKeyboardButton('Upload as Video', callback_data='upload_video_on')]]
 
 @Client.on_message(filters.private & filters.command('upload'))
-async def handle_upload_settings(bot: Client, message: Message):
+async def handle_upload_settings(client, message):
     ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
     upload_type = await db.get_upload_type(message.from_user.id)
     await ms.delete()
@@ -20,19 +20,19 @@ async def handle_upload_settings(bot: Client, message: Message):
         await message.reply_text("Please select the upload format:", reply_markup=InlineKeyboardMarkup(ON))
 
 @Client.on_callback_query(filters.regex('.*?(upload_document_on|upload_video_on).*?'))
-async def set_upload_format(bot: Client, query: CallbackQuery):
+async def set_upload_format(client, query: CallbackQuery):
     data = query.data
     user_id = query.from_user.id
 
     if data == 'upload_document_on':
         await query.message.delete()
-        await db.set_upload_type(user_id, "document")
-        await bot.send_message("Upload format set to **document**.")
+        #await db.set_upload_type(user_id, "document")
+        #await bot.send_message("Upload format set to **document**.")
    
     elif data == 'upload_video_on':
         await query.message.delete()
-        await db.set_upload_type(user_id, "video")
-        await bot.send_message("Upload format set to **video**.")
+       # await db.set_upload_type(user_id, "video")
+      #  await bot.send_message("Upload format set to **video**.")
       
 
 from pyrogram import Client, filters
