2fad4b4296a52e30e19a1cfef621c496981cce5d
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-04-30 21:30:04 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -79,8 +79,14 @@ app = Client("test", api_id=Config.STRING_API_ID, api_hash=Config.STRING_API_HAS
 #       )
 
 # Define the callback for the 'upload' buttons
-@Client.on_callback_query(filters.private & (filters.document | filters.audio | filters.video))
-async def doc(bot, update):
+@Client.on_message(filters.private & (filters.document | filters.audio | filters.video))
+async def rename_start(client, message):
+    file = getattr(message, message.media.value)
+    filename = file.file_name  
+    if file.file_size > 2000 * 1024 * 1024:
+        return await message.reply_text("Sorry, this bot doesn't support uploading files bigger than 2GB")
+    else:
+         pass
     # Creating Directory for Metadata
     if not os.path.isdir("Metadata"):
         os.mkdir("Metadata")
