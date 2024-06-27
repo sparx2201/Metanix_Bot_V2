c325dc74f3c7cce6daa47e6772903822182d9dd6
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-27 21:21:58 +0530

    Update remname.py

--- plugins/remname.py
+++ plugins/remname.py
@@ -5,15 +5,13 @@ from helper.database import db  # Assuming db is your Database class instance
 @Client.on_message(filters.private & filters.command('set_remname'))
 async def add_remname(client, message):
     if len(message.command) == 1:
-        return await message.reply_text("**__Give The Remname Text__\n\nExample:- `/set_remname text_pattern1, text_pattern2, ...`**")
+        return await message.reply_text("**__Give The Remname Text__\n\nExample:- `/set_remname text_pattern`**")
     
-    # Get all the text patterns provided in the command
+    # Get the entire text provided in the command
     remname_text = message.text.split(' ', 1)[1]
-    remname_patterns = [pattern.strip() for pattern in remname_text.split(',')]
 
     SnowDev = await message.reply_text("Please Wait ...")
-    for pattern in remname_patterns:
-        await db.set_remname(message.from_user.id, pattern)
+    await db.set_remname(message.from_user.id, remname_text)
     
     await SnowDev.edit("**Remname Text Saved Successfully ✅**")
 
