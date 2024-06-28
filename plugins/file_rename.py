89dd21675c2d1e9295067fbb0166d5f76c472c7d
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-28 21:21:49 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -84,24 +84,25 @@ async def rename(bot, message):
         suffix = await db.get_suffix(message.chat.id)
         space = "-s"
 
-        if suffix is None:
-            if space in prefix:
-                prefix = prefix.replace(space, "")
-                new_filename = add_sprefix_suffix(new_filename_, prefix, suffix)
-            else:
-                new_filename = add_prefix_suffix(new_filename_, prefix, suffix)
-
-        if prefix is None:
-            if space in prefix:
-                suffix = suffix.replace(space, "")
-                new_filename = add_sprefix_suffix(new_filename_, prefix, suffix)
-            else:
-                new_filename = add_prefix_suffix(new_filename_, prefix, suffix)
+        if prefix is None or suffix is None:
+            if suffix is None:
+                if space in prefix:
+                    prefix = prefix.replace(space, "")
+                    new_filename = add_sprefix_suffix(new_filename_, prefix, suffix)
+                else:
+                    new_filename = add_prefix_suffix(new_filename_, prefix, suffix)
+                    
+            if prefix is None:
+                if space in prefix:
+                    suffix = suffix.replace(space, "")
+                    new_filename = add_sprefix_suffix(new_filename_, prefix, suffix)
+                else:
+                    new_filename = add_prefix_suffix(new_filename_, prefix, suffix)
 
         if prefix is None and suffix is None:
             new_filename = add_prefix_suffix(new_filename_, prefix, suffix)
                
-        if space in prefix and space not in suffix:
+        elif space in prefix and space not in suffix:
             prefix = prefix.replace(space, "")
             new_filename = add_sprefix_suffix(new_filename_, prefix, suffix)
         
