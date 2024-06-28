502babe5f3ad1b1cb9815404da7f5678dabbd038
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-28 13:07:30 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -46,7 +46,6 @@ async def rename(bot, message):
         
     prefix = await db.get_prefix(message.chat.id)
     suffix = await db.get_suffix(message.chat.id)
-    space = "-s"
     print(f"Prefix: {prefix}, Suffix: {suffix}")
 
     new_filename_  = file.file_name 
@@ -75,12 +74,6 @@ async def rename(bot, message):
         
     if remname5 and remname5 in new_filename_:
         new_filename_ = new_filename_.replace(remname5, "")  # Remove the remname text from the new filename
-
-    if space and space in prefix:
-        prefix = prefix.replace(space, "")
-
-    if space and space in suffix:
-        suffix = suffix.replace(space, "")
 
         
     try:
