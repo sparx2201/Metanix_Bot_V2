067b945eac237b5da9efacb7d66e39726a1f9f6e
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-29 19:16:02 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -1,16 +1,31 @@
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
-
-
-
-CHECK_ID_KEYBOARD = InlineKeyboardMarkup([
-    [InlineKeyboardButton("Check ID", callback_data="check_id")]
-])
+from helper.database import db
 
 # Handle /id command
 @Client.on_message(filters.private & filters.command('id'))
 async def handle_id_command(client, message):
     user_id = message.from_user.id
+    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+    upload_type = await db.get_upload_type(message.from_user.id)
+    print(f"Current upload type for user_id={message.from_user.id} is {upload_type}")
+    await ms.delete()
+
+    ON = InlineKeyboardMarkup([
+    [InlineKeyboardButton("Set to Document", callback_data="upload_document_on")],
+    [InlineKeyboardButton("Set to Video", callback_data="upload_video_on")]
+])
+
+    if upload_type == "document":
+        await message.reply_text(f"Your current upload format is set to **Document**.", reply_markup=ON)
+        print(f"Reply sent: Current upload format is Document for user_id={message.from_user.id}")
+    elif upload_type == "video":
+        await message.reply_text(f"Your current upload format is set to **Video**.", reply_markup=ON)
+        print(f"Reply sent: Current upload format is Video for user_id={message.from_user.id}")
+    else:
+        await message.reply_text("Please select the upload format:", reply_markup=ON)
+        print(f"Reply sent: User needs to select upload format for user_id={message.from_user.id}")
+        
     print(f"ID command received from user_id={user_id}")
     await message.reply_text(
         "Press the button below to check your ID:",
@@ -24,6 +39,6 @@ async def handle_callback_query(client, query: CallbackQuery):
     user_id = query.from_user.id
     print(f"Callback query received: data={data}, user_id={user_id}")
 
-    if data == "check_id":
-        await query.message.reply_text(f"Your user ID is: {user_id}")
+    if data == "upload_document_on":
+        await query.message.reply_text("Upload format set to **document**.")
         print(f"User ID {user_id} sent in response to callback query")
