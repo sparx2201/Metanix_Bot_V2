21df18af1c1698a105e9955692dbb4d6099c434b
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-29 21:46:20 +0530

    Update id.py

--- plugins/id.py
+++ plugins/id.py
@@ -10,34 +10,7 @@ async def handle_id_command(client, message):
     print(f"Current upload type for user_id={message.from_user.id} is {upload_type}")
     await ms.delete()
     
-    try:
-        
-        DOC = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Document ✔", callback_data="upload_document_on"), 
-             InlineKeyboardButton("Video", callback_data="upload_video_on")],  
-            [InlineKeyboardButton("Close", callback_data="close")]
-        ])
 
-        VID = InlineKeyboardMarkup([
-            [InlineKeyboardButton("Document", callback_data="upload_document_on"), 
-             InlineKeyboardButton("Video ✔", callback_data="upload_video_on")],  
-            [InlineKeyboardButton("Close", callback_data="close")]
-        ])
-
-        if upload_type == "document":
-            await message.reply_text(f"Your current upload format: **Document**.", reply_markup=DOC)
-            print(f"Reply sent: Current upload format is Document for user_id={user_id}")
-        elif upload_type == "video":
-            await message.reply_text(f"Your current upload format: **Video**.", reply_markup=VID)
-            print(f"Reply sent: Current upload format is Video for user_id={user_id}")
-        else:
-            print(f"Unknown upload type for user_id={user_id}")
-            await message.reply_text("Unknown upload type. Please set your upload type.")
-    except Exception as e:
-        print(f"Error handling /id command for user_id={user_id}: {e}")
-        await message.reply_text("An error occurred. Please try again later.")
-    finally:
-        await ms.delete()
 
 # Handle callback queries
 @Client.on_callback_query()
