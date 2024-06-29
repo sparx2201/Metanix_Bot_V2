09a4c2a61f14b7794cf251f6fb79f4faff60009f
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-29 12:38:19 +0530

    Update upload.py

--- plugins/upload.py
+++ plugins/upload.py
@@ -7,35 +7,32 @@ ON = [[InlineKeyboardButton('Upload as Document', callback_data='upload_document
       [InlineKeyboardButton('Upload as Video', callback_data='upload_video_on')]]
 
 
- @Client.on_callback_query()
- async def set_upload_format(client, query: CallbackQuery):
-     data = query.data
-     user_id = query.from_user.id
+@Client.on_callback_query()
+async def set_upload_format(client, query: CallbackQuery):
+    data = query.data
+    user_id = query.from_user.id
 
-     if data == 'upload_document_on':
-         await query.message.delete()
-         await db.set_upload_type(user_id, "document")
-         await message.reply_text("Upload format set to **document**.")
-   
-     elif data == 'upload_video_on':
-         await query.message.delete()
-         await db.set_upload_type(user_id, "video")
-         await message.reply_text("Upload format set to **video**.")
+    if data == 'upload_document_on':
+        await query.message.delete()
+        await db.set_upload_type(user_id, "document")
+        await query.message.reply_text("Upload format set to **document**.")
+    
+    elif data == 'upload_video_on':
+        await query.message.delete()
+        await db.set_upload_type(user_id, "video")
+        await query.message.reply_text("Upload format set to **video**.")
 
-
- @Client.on_message(filters.private & filters.command('upload'))
- async def handle_upload_settings(client, message):
-     ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
-     upload_type = await db.get_upload_type(message.from_user.id)
-     await ms.delete()
-     if upload_type == "document":
-         await message.reply_text(f"Your current upload format is set to **Document**.", reply_markup=InlineKeyboardMarkup(ON))
-     elif upload_type == "video":
-         await message.reply_text(f"Your current upload format is set to **Video**.", reply_markup=InlineKeyboardMarkup(ON))
-     else:
-         await message.reply_text("Please select the upload format:", reply_markup=InlineKeyboardMarkup(ON))
-
-
+@Client.on_message(filters.private & filters.command('upload'))
+async def handle_upload_settings(client, message):
+    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+    upload_type = await db.get_upload_type(message.from_user.id)
+    await ms.delete()
+    if upload_type == "document":
+        await message.reply_text(f"Your current upload format is set to **Document**.", reply_markup=InlineKeyboardMarkup(ON))
+    elif upload_type == "video":
+        await message.reply_text(f"Your current upload format is set to **Video**.", reply_markup=InlineKeyboardMarkup(ON))
+    else:
+        await message.reply_text("Please select the upload format:", reply_markup=InlineKeyboardMarkup(ON))
 
 
 from pyrogram import Client, filters
