d1d79ef4a4559f48971e1a26a9b512f673884574
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-30 15:21:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, CallbackQuery
 from helper.database import db
-from config import Config, Txt, ADMIN
+from config import Config, Txt
 import humanize
 from time import sleep
 
