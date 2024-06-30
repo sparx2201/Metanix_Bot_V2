9c6e290b28aa316a4f30f372e9e989a66991b28a
rahul torat <158738968+sparx2201@users.noreply.github.com>
2024-06-30 13:42:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -150,6 +150,6 @@ async def handle_id_command(client, message):
         
 @Client.on_message(filters.private & filters.command('imp'))
 async def imp(client, message):
-    await message.reply_text(f"hi", reply_markup=CLS)
+    await message.reply_text("hi", reply_markup=CLS)
 
 
